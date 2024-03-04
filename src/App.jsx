822aa63e1e6586350ab149f451bffa1fb1ff9f0b
Albatraoz12 <dimosthenis.emmanouil@chasacademy.se>
2024-03-04 21:09:25 +0100

    get users geolocation and store it into states

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
+import { useEffect, useState } from 'react';
 import './index.scss';
 
 function App() {
+  const [lat, setLat] = useState('');
+  const [long, setLong] = useState('');
+  useEffect(() => {
+    const getLocation = async () => {
+      navigator.geolocation.getCurrentPosition(function (position) {
+        setLat(position.coords.latitude);
+        setLong(position.coords.longitude);
+      });
+      console.log(lat, long);
+    };
+    getLocation();
+  }, [lat, long]);
+
   return (
     <>
       <div>
-        <h1>Hello World</h1>
+        <h1>Hello Weather App</h1>
       </div>
     </>
   );
