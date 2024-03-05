aa7987d64da169dad93dfb0499fa6dc5698904a2
Albatraoz12 <dimosthenis.emmanouil@chasacademy.se>
2024-03-05 20:33:59 +0100

    remove h1

--- src/App.jsx
+++ src/App.jsx
@@ -27,8 +27,7 @@ function App() {
   return (
     <>
       <div>
-        <CurrentForeCast currentData={data.current} />
-        <h1>Hello Weather App</h1>
+        <CurrentForeCast currentData={data} />
       </div>
     </>
   );
