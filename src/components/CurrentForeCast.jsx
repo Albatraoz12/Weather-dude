648e31323ac970cb951dded475e9450d39edb8de
Albatraoz12 <dimosthenis.emmanouil@chasacademy.se>
2024-03-05 21:01:57 +0100

    Enhance CurrentForeCast component to display weather information and city name

--- src/components/CurrentForeCast.jsx
+++ src/components/CurrentForeCast.jsx
@@ -1,10 +1,16 @@
-import { formatedDate } from '../lib/helper';
-
+import { formatedDate, getCity } from '../lib/helper';
 const CurrentForeCast = ({ currentData }) => {
   return (
     <section>
+      <figure>
+        <img
+          src={`http://openweathermap.org/img/wn/${currentData.current.weather[0].icon}@2x.png`}
+          alt={`${currentData.current.weather[0].description}`}
+        ></img>
+      </figure>
       <div className='location'>
-        <h1>{currentData.timezone}</h1>
+        <span>{currentData.current.temp.toFixed()}</span>
+        <h1>{getCity(currentData.timezone)}</h1>
         <p>Today | {formatedDate(currentData.current.dt)}</p>
       </div>
     </section>
