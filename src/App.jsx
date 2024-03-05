d44f264e3441b6ba41402bd12a8d7d8d5091b05d
Albatraoz12 <dimosthenis.emmanouil@chasacademy.se>
2024-03-05 20:52:39 +0100

    Ensure proper handling of initial data state in App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './index.scss';
 import CurrentForeCast from './components/CurrentForeCast';
 
 function App() {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState(null); // Initialize data state to null
   const [lat, setLat] = useState('');
   const [long, setLong] = useState('');
   useEffect(() => {
@@ -12,24 +12,26 @@ function App() {
         setLat(position.coords.latitude);
         setLong(position.coords.longitude);
       });
-      if (lat && long) {
-        const apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&appid=${
-          import.meta.env.VITE_API_KEY
-        }`;
-        const response = await fetch(apiUrl);
-        const info = await response.json();
-        setData(info);
-      }
     };
     getLocation();
+  }, []);
+
+  useEffect(() => {
+    if (lat !== '' && long !== '') {
+      const apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&appid=${
+        import.meta.env.VITE_API_KEY
+      }`;
+      fetch(apiUrl)
+        .then((response) => response.json())
+        .then((info) => setData(info))
+        .catch((error) => console.error('Error fetching weather data:', error));
+    }
   }, [lat, long]);
 
   return (
-    <>
-      <div>
-        <CurrentForeCast currentData={data} />
-      </div>
-    </>
+    <div>
+      {data ? <CurrentForeCast currentData={data} /> : <p>Loading...</p>}
+    </div>
   );
 }
 
