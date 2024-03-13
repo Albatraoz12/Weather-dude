1ed674652b3f9276af2bece7df73580347eee643
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.com>
2024-03-13 14:20:03 +0100

    style buttons to change F to C for mobile view

--- src/components/CurrentForeCast.jsx
+++ src/components/CurrentForeCast.jsx
@@ -12,8 +12,7 @@ const CurrentForeCast = ({ currentData, units, setUnits }) => {
       </figure>
       <div className='location'>
         <p className='text-6xl'>
-          {currentData.current.temp.toFixed()}°{' '}
-          {units === 'metric' ? 'C' : '°F'}{' '}
+          {currentData.current.temp.toFixed()}° {units === 'metric' ? 'C' : 'F'}{' '}
         </p>
         <h1 className='text-3xl font-bold my-2'>
           {getCity(currentData.timezone)}
@@ -22,11 +21,19 @@ const CurrentForeCast = ({ currentData, units, setUnits }) => {
           Today | {formatedDate(currentData.current.dt)}
         </p>
       </div>
-      <div>
-        <button type='button' onClick={() => setUnits('metric')}>
+      <div className='flex justify-center items-center gap-3'>
+        <button
+          type='button'
+          onClick={() => setUnits('metric')}
+          className='rounded-full border border-white px-4 py-2'
+        >
           C
         </button>
-        <button type='button' onClick={() => setUnits('imperial')}>
+        <button
+          type='button'
+          onClick={() => setUnits('imperial')}
+          className='rounded-full border border-white px-4 py-2'
+        >
           F
         </button>
       </div>
